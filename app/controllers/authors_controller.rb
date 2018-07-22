4cbc1597b3b11ad730dc86b2d594ff7a2b5adab6
Meredith Reece <mardebugger@gmail.com>
2018-07-22 03:42:49 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,16 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    # @author = Author.create!(author_params)
+    # 
+    # redirect_to author_path(@author)
+    @author = Author.new
+    if @author.valid?
+      @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
