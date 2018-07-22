123855ccd381737822188dd37409c665810bac1f
Meredith Reece <mardebugger@gmail.com>
2018-07-22 03:47:03 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,6 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.new(post_params)
-    if @post.valid?
-      @post.save
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end
 
   end
 
